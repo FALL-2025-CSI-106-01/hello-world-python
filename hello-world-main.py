ed047adf65b3b4ffbd837ffc1f56aaf3f37b5bbc
Nics-Github <nicholasschwab@gmail.com>
2025-08-17 11:45:20 +0000

    Updating dev container

--- hello-world-main.py
+++ hello-world-main.py
@@ -1,6 +1,14 @@
 # Pablo Frank
+# Edits by Nicholas Schwab
 # Hi, this is a simple python program to get you started
 # All lines that begin with a "#" are comments (not executed)
 
 # The line show below is a python instruction that will be executed
-print("hello world!")
+print()
+
+# Strings are bits of text that the computer does not execute. 
+# By themselves strings don't do much. 
+"Hello World"
+
+# When added to a "print" you can print them. Put "Hello World" into the print() 
+# function. 
